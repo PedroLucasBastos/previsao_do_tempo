a7f52e8abde249c93557b9cdf7ebea1e7a4d3192
Pedro Bastos <pedro19792001@gmail.com>
2024-11-11 16:31:29 -0300

    Informacaoes de tempos e css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import InformacaoTempo from './components/informacaoTempo/informacaoTempo'
 
 function App() {
-  const [tempo, setTempo] = useState({})
+  const [tempo, setTempo] = useState()
   const inputRef = useRef()
 
   async function BuscarCidade() {
@@ -13,6 +13,7 @@ function App() {
     const cidade = inputRef.current.value
     const key = "62ae6a876cd9334a11e4a58c140edd56"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`
+    const url5dias = `https:api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${key}`
 
     const dados = await axios.get(url)
 
@@ -23,11 +24,12 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='container'>
+      <h1>Previsão do Tempo</h1>
         <input ref={inputRef} type='text' placeholder='Digite a cidade desejada'/>
         <button onClick={BuscarCidade}>Buscar</button>
 
-        <InformacaoTempo tempo={tempo} />
+        {tempo && <InformacaoTempo tempo={tempo} />} {/* Isso serve como um IF, para não deixar a aplicação tentar iniciar sem dados*/}
       </div>
     </>
   )
--- src/components/informacaoTempo/informacaoTempo.jsx
+++ src/components/informacaoTempo/informacaoTempo.jsx
@@ -1,9 +1,26 @@
+/* eslint-disable react/prop-types */
+import './informacaoTempo.css'
+
 function InformacaoTempo({ tempo }) {
   console.log(tempo);
 
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className="tempo-container">
+      <h2>{tempo.name}</h2>
+      <div className="tempo-info">
+        <img
+          src={`http://openweathermap.org/img/wn/${tempo.weather[0].icon}.png`}
+        />
+        <p className="temperatura">{Math.round(tempo.main.temp)} °C</p>
+      </div>
+      <div>
+        <p className="descricao">{tempo.weather[0].description}</p>
+      </div>
+      <div className="detalhes">
+        <p>Sensação termica:{Math.round(tempo.main.feels_like)}</p>
+        <p>Pressão: {tempo.main.pressure}</p>
+        <p>Umidade: {tempo.main.humidity}%</p>
+      </div>
     </div>
   );
 }
